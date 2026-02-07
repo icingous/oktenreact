1e173a3e2b3286c6f80d76b6ffda0bd5eea804ba
vmikov <vmikov@ukr.net>
2026-02-07 17:29:03 +0200

    adds react/homework1/#gi01MZ6v homework

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import './App.css';
 
+import { coursesTitleArray } from './assets/arrays';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>FE. ДЗ 1</h1>
+      <section className='homework'>
+        <h2>#gi01MZ6v</h2>
+
+        <ul className='course-titles'>
+          {coursesTitleArray.map((item, i) => (
+            <li key={i}>{item}</li>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
 }
 
 export default App;
