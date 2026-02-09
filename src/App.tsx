f255f75abd049ff361f70f8e6fe0887fb19896d3
vmikov <vmikov@ukr.net>
2026-02-09 22:10:25 +0200

    removes debugging

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,8 @@ import Todos from './components/todos/Todos';
 import Posts from './components/posts/Posts';
 import Comments from './components/comments/Comments';
 import './App.css';
-// import { getComments } from './services/api.service';
-// import { useEffect } from 'react';
 
 function App() {
-  // useEffect(() => {
-  //   getComments().then((data) => console.log(data));
-  // }, []);
-
   return (
     <>
       <Comments />
