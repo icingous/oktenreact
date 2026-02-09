5d8b05a511503373597d470189f8305769244ecb
vmikov <vmikov@ukr.net>
2026-02-09 17:26:53 +0200

    improves task's #x7sQQ3cJ3 styling

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -8,9 +8,9 @@ type PropTypes = {
 const Post: FC<PropTypes> = ({ post: { id, userId, title, body } }) => {
   return (
     <div className='p-2 flex flex-col gap-2 odd:bg-amber-200 even:bg-gray-100'>
-      <h3 className='font-bold text-2xl'>{title}</h3>
+      <h3 className='font-semibold text-2xl'>{title}</h3>
       <p className='italic'>{body}</p>
-      <p className='flex gap-3.5 font-bold text-sm'>
+      <p className='flex gap-3.5 font-bold text-xs'>
         <span>{`id: ${id}`}</span>
         <span>{`userId: ${userId}`}</span>
       </p>
